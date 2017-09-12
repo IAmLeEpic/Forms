36db3ba1f1aead9bd5477ce04e58fd2a6551babe
IAmLeEpic <te15.jonathan.sjogren@umea.nti.se>
2017-09-12 15:02:48 +0200

    First Commit
    Skapade saker

---
+++ form.php
@@ -0,0 +1,24 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hej</title>
+	</head>
+	<body>
+
+	<form action="welcome.php" method="POST">
+	Name <br>	
+	<input type="text" name="name"><br>
+
+	Date of birth <br>
+	<input type="Date" name="birth"><br>
+	
+	Gender <br>
+	<input type="radio" name="gender" value="male"> Male
+	<input type="radio" name="gender" value="female"> Female 
+	<br>
+	<input type="submit">
+	</form>
+
+	</body>
+	</html>
---
+++ welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome</title>
+</head>
+<body>
+
+Welcome <?php echo $_POST["name"];?><br>
+Your date of birth is <?php echo $_POST["birth"]; ?>
+
+</body>
+</html>
